b2e475b25c3d6c779ee780bcd4634c6d8242aa0a
Vladimir Ivakin <vl.ivakin@yandex.ru>
2016-11-08 22:34:38 +0700

    Add BFS

--- MyStrategy.py
+++ MyStrategy.py
@@ -1,46 +1,234 @@
-from model.ActionType import ActionType
 from model.Game import Game
 from model.Move import Move
 from model.Wizard import Wizard
 from model.World import World
 from model.LaneType import LaneType
-from random import randrange
+from random import randrange, uniform
+from math import sin
+from time import time
+
+
+def clamp(n, smallest, largest):
+        return max(smallest, min(n, largest))
 
 
 class MyStrategy:
 
     def __init__(self):
         self.lane = None
+        self.x, self.y = None, None
         self.lane_point_index = 0
-        self.lane_BOTTOM = []
-        self.lane_MIDDLE = []
-        self.lane_TOP = []
-        self.step_coordinates = None
+        self.lane_array_BOTTOM = []
+        self.lane_array_MIDDLE = []
+        self.lane_array_TOP = []
+        self.target_point_x, self.target_point_y = None, None
+        self.step_point_x, self.step_point_y = None, None
+        self.lane_array_BOTTOM = [[250, 3800],
+                                  [500, 3800],
+                                  [750, 3800],
+                                  [1000, 3800],
+                                  [1250, 3800],
+                                  [1500, 3800],
+                                  [1750, 3800],
+                                  [2000, 3800],
+                                  [2250, 3800],
+                                  [2500, 3800],
+                                  [2750, 3800],
+                                  [3000, 3800],
+                                  [3250, 3800],
+                                  [3500, 3800],
+                                  [3750, 3600],
+                                  [3750, 3300],
+                                  [3750, 3000],
+                                  [3750, 2750],
+                                  [3750, 2500],
+                                  [3750, 2250],
+                                  [3750, 2000],
+                                  [3750, 1750],
+                                  [3750, 1500],
+                                  [3750, 1250],
+                                  [3750, 1000],
+                                  [3750, 750],
+                                  [3750, 500]]
 
     def move(self, me: Wizard, world: World, game: Game, move: Move):
-        if world.tick_index == 1:
-            self.init()
+        if world.tick_index == 0:
+            self.init(me)
             return
+        c = time()
+        self.x = me.x
+        self.y = me.y
 
-        move.speed = game.wizard_forward_speed
-        move.strafe_speed = game.wizard_strafe_speed
-        move.turn = game.wizard_max_turn_angle
-        move.action = ActionType.MAGIC_MISSILE
+        if me.get_distance_to(self.target_point_x, self.target_point_y) < 200:
+            self.map_master(1)
 
-    def init(self):
-        self.lane = randrange(LaneType.BOTTOM, LaneType.MIDDLE, LaneType.TOP)
-        self.lane_BOTTOM = [[0, 800],
-                            [500, 800],
-                            [1000, 800],
-                            [1500, 800],
-                            [2000, 800],
-                            [2500, 800],
-                            [3000, 800],
-                            [3500, 800]]
+        self.step_point_x, self.step_point_y = self.find_way(world, me)
+        self.go(me, move, game, world.tick_index)
 
-    def map_master(self, lane, lane_point_index, direction):
+        # move.action = ActionType.MAGIC_MISSILE
 
+        # debug
+        if world.tick_index % 30 == 0:
+            # print(move.turn)
+            print("TIME: ", time() - c)
+            print("Tick: ", world.tick_index)
+            print("X, Y coord: ", self.x, self.y)
+            print("Target points: ", self.target_point_x, self.target_point_y)
+            print("Step points: ", self.step_point_x, self.step_point_y)
+            print("Distance: ", me.get_distance_to(self.target_point_x, self.target_point_y))
+
+    def init(self, me):
+        # self.lane = randrange(LaneType.BOTTOM, LaneType.MIDDLE, LaneType.TOP)
+        self.lane = LaneType.BOTTOM
+        self.x = me.x
+        self.y = me.y
+        self.target_point_x, self.target_point_y = self.x, self.y
+
+    def map_master(self, direction):
+        lane_array = None
+
+        if self.lane == LaneType.BOTTOM:
+            lane_array = self.lane_array_BOTTOM
+        elif self.lane == LaneType.MIDDLE:
+            lane_array = self.lane_array_MIDDLE
+        elif self.lane == LaneType.TOP:
+            lane_array = self.lane_array_TOP
+
+        lane_array_len = len(lane_array)
         if direction == 1:
-            pass
+            if self.lane_point_index != lane_array_len - 1:
+                self.lane_point_index += 1
         else:
-            pass
+            if self.lane_point_index != 0:
+                self.lane_point_index -= 1
+        self.target_point_x, self.target_point_y = lane_array[self.lane_point_index]
+
+    def go(self, me: Wizard, move: Move, game: Game, tick):
+        move.turn = clamp(me.get_angle_to(self.step_point_x, self.step_point_y),
+                          -game.wizard_max_turn_angle, game.wizard_max_turn_angle)
+        move.speed = game.wizard_forward_speed * (1 - 0.5 * move.turn / game.wizard_max_turn_angle)
+        move.strafe_speed += game.wizard_strafe_speed * sin(0.05 * tick) * 0.2
+        # move.strafe_speed = clamp(move.strafe_speed, -game.wizard_strafe_speed, game.wizard_strafe_speed)
+
+    def find_way(self, world, me):
+        width = 40
+        n, m = int(800 / width / 2), int(800 / width / 2 - 1)
+        k = int(800 / width - 1)
+        stop_x = int(clamp((self.target_point_x - self.x + 400) // width, 0, k))
+        stop_y = int(clamp((self.target_point_y - self.y + 400) // width, 0, k))
+        area = [[0 for i in range(int(800 / width))] for j in range(int(800 / width))]
+        for i in world.buildings:
+            if me.get_distance_to(i.x, i.y) < 400:
+                area[int((i.x - self.x + 400) // width)][int((i.y - self.y + 400) // width)] = 1
+        for i in world.minions:
+            if me.get_distance_to(i.x, i.y) < 400:
+                area[int((i.x - self.x + 400) // width)][int((i.y - self.y + 400) // width)] = 1
+        for i in world.wizards:
+            if me.get_distance_to(i.x, i.y) < 400:
+                area[int((i.x - self.x + 400) // width)][int((i.y - self.y + 400) // width)] = 1
+        for i in world.trees:
+            if me.get_distance_to(i.x, i.y) < 400:
+                area[int((i.x - self.x + 400) // width)][int((i.y - self.y + 400) // width)] = 1
+        area[m][m] = 0
+        area[m][n] = 0
+        area[n][m] = 0
+        area[n][n] = 0
+        if self.x < self.target_point_x:
+            if self.y < self.target_point_y:
+                start_x, start_y = n, n
+            else:
+                start_x, start_y = m, n
+        else:
+            if self.y < self.target_point_y:
+                start_x, start_y = m, n
+            else:
+                start_x, start_y = m, m
+        points = [[start_x, start_y]]
+        index = 1
+        break_flag = True
+        # bfs
+        while break_flag and len(points) != 0:
+            index += 1
+            add_to_points = []
+            for i in points:
+                x, y = i[0], i[1]
+                if x == stop_x and y == stop_y:
+                    break_flag = False
+                    break
+                area[x][y] = index - 1
+                if x != k:
+                    if area[x+1][y] == 0:
+                        area[x+1][y] = index
+                        add_to_points.append([x+1, y])
+                    if y != k:
+                        if area[x+1][y+1] == 0:
+                            area[x+1][y+1] = index
+                            add_to_points.append([x+1, y+1])
+                    if y != 0:
+                        if area[x+1][y-1] == 0:
+                            area[x+1][y-1] = index
+                            add_to_points.append([x+1, y-1])
+                if x != 0:
+                    if area[x-1][y] == 0:
+                        area[x-1][y] = index
+                        add_to_points.append([x-1, y])
+                    if y != k:
+                        if area[x-1][y+1] == 0:
+                            area[x-1][y+1] = index
+                            add_to_points.append([x-1, y+1])
+                    if y != 0:
+                        if area[x-1][y-1] == 0:
+                            area[x-1][y-1] = index
+                            add_to_points.append([x-1, y-1])
+                if y != k:
+                    if area[x][y+1] == 0:
+                        area[x][y+1] = index
+                        add_to_points.append([x, y+1])
+                if y != 0:
+                    if area[x][y-1] == 0:
+                        area[x][y-1] = index
+                        add_to_points.append([x, y-1])
+            points = add_to_points
+
+        # find way in array
+        way = []
+        index = area[stop_x][stop_y]
+        # print("INDEX ", index)
+        x, y = stop_x, stop_y
+        if index > 1:
+            while index != 2:
+                index -= 1
+                if x != k:
+                    if area[x + 1][y] == index:
+                        x, y = x + 1, y
+                        continue
+                    if y != k:
+                        if area[x + 1][y + 1] == index:
+                            x, y = x + 1, y + 1
+                            continue
+                    if y != 0:
+                        if area[x + 1][y - 1] == index:
+                            x, y = x + 1, y - 1
+                            continue
+                if x != 0:
+                    if area[x - 1][y] == index:
+                        x, y = x - 1, y
+                        continue
+                    if y != k:
+                        if area[x - 1][y + 1] == index:
+                            x, y = x - 1, y + 1
+                            continue
+                    if y != 0:
+                        if area[x - 1][y - 1] == index:
+                            x, y = x - 1, y - 1
+                            continue
+                if y != k:
+                    if area[x][y + 1] == index:
+                        x, y = x, y + 1
+                        continue
+                if y != 0:
+                    if area[x][y - 1] == 0:
+                        x, y = x, y - 1
+                        continue
+        return x * width - 400 + self.x, y * width - 400 + self.y
+
