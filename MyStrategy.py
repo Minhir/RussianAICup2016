ae905e29aab3f321fefc7291ee7a9bfb88399b0a
Vladimir Ivakin <vl.ivakin@yandex.ru>
2016-11-10 00:53:34 +0700

    Add debug visualization

--- MyStrategy.py
+++ MyStrategy.py
@@ -7,14 +7,17 @@ from model.ActionType import ActionType
 from model.Faction import Faction
 from random import randrange, uniform
 from math import sin, fabs
+import copy
 from time import time
 
 try:
     from debug_client import DebugClient
+    from debug_client import Color
 except:
     debug = None
 else:
     debug = DebugClient()
+
 
 def clamp(n, smallest, largest):
         return max(smallest, min(n, largest))
@@ -23,6 +26,9 @@ def clamp(n, smallest, largest):
 class MyStrategy:
 
     def __init__(self):
+        self.area = [] # debug
+        self.width = None
+        #
         self.lane = None
         self.x, self.y = None, None
         self.lane_point_index = 0
@@ -38,65 +44,44 @@ class MyStrategy:
         self.faction = None
         self.enemy_faction = None
         self.lane_array_BOTTOM = [[250, 3800],
-                                  [500, 3800],
-                                  [750, 3800],
+                                  [600, 3800],
                                   [1000, 3800],
-                                  [1250, 3800],
-                                  [1500, 3800],
-                                  [1750, 3800],
-                                  [2000, 3800],
-                                  [2250, 3800],
-                                  [2500, 3800],
-                                  [2750, 3800],
+                                  [1400, 3800],
+                                  [1800, 3800],
+                                  [2200, 3800],
+                                  [2600, 3800],
                                   [3000, 3800],
-                                  [3250, 3800],
-                                  [3500, 3800],
-                                  [3850, 3600],
+                                  [3400, 3800],
+                                  [3600, 3700],
+                                  [3800, 3600],
                                   [3850, 3300],
                                   [3850, 3000],
-                                  [3850, 2750],
-                                  [3850, 2500],
-                                  [3850, 2250],
-                                  [3850, 2000],
-                                  [3850, 1750],
-                                  [3850, 1500],
-                                  [3850, 1250],
+                                  [3850, 2600],
+                                  [3850, 2200],
+                                  [3850, 1800],
+                                  [3850, 1400],
                                   [3850, 1000],
-                                  [3850, 750],
-                                  [3850, 500]]
+                                  [3850, 600],
+                                  [3850, 200],]
 
     def move(self, me: Wizard, world: World, game: Game, move: Move):
         if world.tick_index == 0:
             self.init(me)
             return
-        c = time()
+        #c = time()
         self.x = me.x
         self.y = me.y
 
         if me.life < me.max_life * 0.3:
             self.map_master(-1)
-        if me.get_distance_to(self.target_point_x, self.target_point_y) < 200:
+        elif me.get_distance_to(self.target_point_x, self.target_point_y) < 200:
             self.map_master(1)
 
         self.step_point_x, self.step_point_y = self.find_way(world, me)
         self.stuck_check()
         self.attack(move, game, me, self.situation_analysis(world, me, move))
         self.go(me, move, game, world.tick_index)
-
-        # debug
-        if debug:
-            with debug.post() as dbg:
-                dbg.post(debug.circle(self.step_point_x, self.step_point_y, 10, "r"))
-
-        if world.tick_index % 1 == 0:
-            # print(move.turn)
-            if time() - c > 0.001:
-                print("TIME: ", time() - c)
-            '''print("Tick: ", world.tick_index)
-            print("X, Y coord: ", self.x, self.y)
-            print("Target points: ", self.target_point_x, self.target_point_y)
-            print("Step points: ", self.step_point_x, self.step_point_y)
-            print("Distance: ", me.get_distance_to(self.target_point_x, self.target_point_y))'''
+        self.debug_func(world)
 
     def attack(self, move, game, me, enemy):
         if enemy is None:
@@ -179,20 +164,20 @@ class MyStrategy:
             move.turn = clamp(me.get_angle_to(self.step_point_x, self.step_point_y),
                               -game.wizard_max_turn_angle, game.wizard_max_turn_angle)
             move.speed = game.wizard_forward_speed * (1 - 0.5 * move.turn / game.wizard_max_turn_angle)
-            move.strafe_speed += game.wizard_strafe_speed * sin(0.05 * tick) * 0.2
+            #move.strafe_speed += game.wizard_strafe_speed * sin(0.05 * tick) * 0.2
         elif self.status == "fight":
             pass
         elif self.status == "stuck":
             if self.stuck_start_tick is None:
                 self.stuck_start_tick = tick
-            elif tick - self.stuck_start_tick > 30:
+            elif tick - self.stuck_start_tick > 20:
                 self.status = "go"
                 self.stuck_start_tick = None
             move.speed = -game.wizard_forward_speed
             move.turn = uniform(-game.wizard_max_turn_angle, game.wizard_max_turn_angle)
 
     def find_way(self, world, me):
-        width = 80
+        width = 100
         n, m = int(800 / width / 2), int(800 / width / 2 - 1)
         k = int(800 / width - 1)
         stop_x = int(clamp((self.target_point_x - self.x + 400) // width, 0, k))
@@ -214,6 +199,8 @@ class MyStrategy:
         area[m][n] = 0
         area[n][m] = 0
         area[n][n] = 0
+        self.area = copy.deepcopy(area) # debug
+        self.width = width # debug
         if self.x < self.target_point_x:
             if self.y < self.target_point_y:
                 start_x, start_y = n, n
@@ -274,7 +261,6 @@ class MyStrategy:
         # find way in array
         way = []
         index = area[stop_x][stop_y]
-        # debug print("INDEX ", index)
         x, y = stop_x, stop_y
         if index > 1:
             while index != 2:
@@ -312,4 +298,34 @@ class MyStrategy:
                         x, y = x, y - 1
                         continue
 
-        return x * width - 400 + self.x, y * width - 400 + self.y
+        return x * width - 400 + self.x + width / 2, y * width - 400 + self.y + width / 2
+
+    def debug_func(self, world):
+        if debug:
+            area = self.area
+            width = self.width
+            with debug.pre() as dbg:
+                for i in self.lane_array_BOTTOM:
+                    radius = 5
+                    if i == self.lane_array_BOTTOM[self.lane_point_index]:
+                        radius = 25
+                    dbg.fill_circle(i[0], i[1], radius, Color(r=1.0, g=0.0, b=0.0))
+                for i in range(len(area)):
+                    for j in range(len(area)):
+                        if area[i][j] == 0:
+                            dbg.rect(i * width - 400 + self.x, j * width - 400 + self.y,
+                                     (i + 1) * width - 400 + self.x, (j + 1) * width - 400 + self.y,
+                                     Color(r=0.0, g=0.0, b=0.0))
+                        else:
+                            dbg.fill_rect(i * width - 400 + self.x, j * width - 400 + self.y,
+                                          (i + 1) * width - 400 + self.x, (j + 1) * width - 400 + self.y,
+                                          Color(r=0.0, g=0.0, b=0.0))
+            with debug.post() as dbg:
+                dbg.fill_circle(self.step_point_x, self.step_point_y, 10, Color(r=0.0, g=1.0, b=0.0))
+
+
+
+
+
+
+
